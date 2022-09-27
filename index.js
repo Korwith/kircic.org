6cfed7aaf3f8290640359cd6fdc79f9194b40ab3
JennetteMcCurdy <80077493+JennetteMcCurdy@users.noreply.github.com>
2022-09-27 13:59:38 -0400

    delete element instead of hiding it

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var enter = document.getElementById('enter');
 var links = document.getElementById('links');
 
 enter.onclick = function() {
-  enter.style.visibility = 'hidden';
+  enter.remove()
   links.style.visibility = 'visible';
 };
