67ff6391d9a33751edce13e0fdf054603571f094
Korwith <80077493+Korwith@users.noreply.github.com>
2023-10-22 12:07:15 -0400

    Add files via upload

--- indexsub/index.js
+++ indexsub/index.js
@@ -4,18 +4,22 @@ var buttontolink = {
     'kirhubv1': 'sub/legacy/me/index.html',
     'enchant': 'sub/legacy/enchant/index.html',
     'math': 'sub/math.html',
-    'flappy': 'sub/flappy/game.html'
+    'flappy': 'sub/flappy/game.html',
+    'rhythm2': 'sub/newrhythm/game.html'
 }
 
 for (var key in buttontolink) {
     var button = document.getElementById(key);
     button.onclick = function (currentKey) {
         return function () {
-            window.open(buttontolink[currentKey], '_blank');
+            //window.open(buttontolink[currentKey], '_blank');
+            openPreview(buttontolink[currentKey]);
         };
     }(key);
 }
 
+var gameframe = document.querySelector('#playhere');
+var gameframeoverlay = document.querySelector('#playoverlay');
 var home = document.querySelector('.home')
 var discord = document.querySelector('.discord');
 var youtube = document.querySelector('.youtube');
@@ -35,4 +39,34 @@ accounts.onclick = function () {
 
 home.onclick = function () {
     alert('you found me');
+}
+
+function checkMobile() {
+    return (window.innerWidth < 767);
+}
+
+function openPreview(url) {
+    if (checkMobile()) {
+        window.open(url, '_blank');
+    } else {
+        gameframe.src = url;
+        gameframe.classList.remove('hidden');
+        gameframeoverlay.classList.remove('hidden');
+    }
+}
+
+var previewclose = document.querySelector('.close');
+var previewopen = document.querySelector('.newtab');
+
+previewclose.onclick = function() {
+    gameframe.classList.add('hidden');
+    gameframeoverlay.classList.add('hidden');
+
+    setTimeout(function() {
+        gameframe.removeAttribute('src');
+    }, 500);
+}
+
+previewopen.onclick = function() {
+    window.open(gameframe.src, '_blank');
 }
