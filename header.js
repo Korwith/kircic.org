78eb3ec42d2cffd26d6de18a77876ac8bd0f122a
Korwith <80077493+Korwith@users.noreply.github.com>
2023-10-28 15:17:58 -0400

    Add files via upload

---
+++ header.js
@@ -0,0 +1,14 @@
+var discord = document.querySelector('.discord');
+discord.onclick = function() {
+    window.open('https://discord.gg/p8ZZXZqnag', '_blank');
+}
+
+var home = document.querySelector('.home');
+home.onclick = function() {
+    window.open('index.html');
+}
+
+var accounts = document.querySelector('.accounts');
+accounts.onclick = function() {
+    window.open('services.html');
+}
