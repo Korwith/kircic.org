19874949bd244dc8647bc260ee92526da66a53f9
Korwith <80077493+Korwith@users.noreply.github.com>
2023-12-16 18:31:57 -0500

    Update headeronly.js

--- headeronly.js
+++ headeronly.js
@@ -1,47 +1,34 @@
-var pageTitle = document.querySelector('#pageTitle')
-var homeIcon = document.querySelector('#homeIcon');
-var mobieDiscordButton = document.querySelector('#mobileDiscordButton');
-var homeButton = document.querySelector('.homeButton');
+var mobileDiscord = document.querySelector('#mobileDiscordButton');
+var discordButton = document.querySelector('.discordButton');
+var homeButton = document.querySelector('#homeButton');
 var updatesButton = document.querySelector('.updatesButton');
 var accountsButton = document.querySelector('.accountsButton');
 var peopleButton = document.querySelector('.peopleButton');
-var discordButton = document.querySelector('.discordButton');
+var musicButton = document.querySelector('.musicButton');
 
-var buttonToLink = [
-    {button: homeIcon, link: 'index.html'},
-    {button: homeButton, link: 'index.html'},
-    {button: updatesButton, link: 'updates.html'},
-    {button: accountsButton, link: 'services.html'},
-    {button: peopleButton, link: 'contributors.html'}
-]
-
-var externalLinks = [
-    {button: discordButton, link: 'https://discord.gg/invite/p8ZZXZqnag'},
-    {button: mobieDiscordButton, link: 'https://discord.gg/invite/p8ZZXZqnag'}
-]
-
-for (var i = 0; i < buttonToLink.length; i++) {
-    (function(i) {
-        var thisData = buttonToLink[i];
-        var thisButton = thisData.button;
-        var thisLink = thisData.link;
-
-        thisButton.onclick = function() {
-            window.location.href = thisLink;
-        }
-    })(i);
+homeButton.onclick = function() {
+    window.location.href = 'index.html';
 }
 
-for (var i = 0; i < externalLinks.length; i++) {
-    (function(i) {
-        var thisData = externalLinks[i];
-        var thisButton = thisData.button;
-        var thisLink = thisData.link;
-
-        thisButton.onclick = function() {
-            window.open(thisLink, '_blank');
-        }
-    })(i)
+updatesButton.onclick = function() {
+    window.location.href = 'updates.html';
 }
 
-pageTitle.innerHTML = document.title.split('|')[0];
+accountsButton.onclick = function() {
+    window.location.href = 'accounts.html';
+}
+
+peopleButton.onclick = function() {
+    window.location.href = 'contributors.html';
+}
+
+musicButton.onclick = function() {
+    window.location.href = 'music.html';
+}
+
+function discord() {
+    window.open('https://discord.com/invite/p8ZZXZqnag', '_blank');
+}
+
+discordButton.onclick = discord;
+mobileDiscord.onclick = discord;
